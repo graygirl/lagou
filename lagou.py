f89afd8fed0b4a049f354754ffdd288249655bca
graygirl <1937215716@qq.com>
2017-10-19 20:35:39 +0800

    增加请求模块

--- lagou.py
+++ lagou.py
@@ -1,7 +1,16 @@
 # -*- coding:utf-8 -*-
 import urllib
+import urllib.request
 import re
 import xlwt
+
+from urllib import request
+with request.urlopen("https://www.lagou.com/jobs/list_python?city=%E5%85%A8%E5%9B%BD&cl=false&fromSearch=true&labelWords=&suginput=%E7%8E%8B%E5%86%AC%202017/9/26%2018:27:45") as f:
+    data = f.read()
+    print('Status: ',f.status,f.reason)
+    for k,v in f.getheaders():
+        print('%s: %s' %(k,v))
+    print('Data: ',data.decode('utf-8'))
 
 
 
